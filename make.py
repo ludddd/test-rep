cf74a9e518ce16b712dbcd2aa48cff6d8b6a7ea2
Anatoly Ahmedov <ludddddd@gmail.com>
2016-06-22 16:39:21 +0300

    test unity project and make file

---
+++ make.py
@@ -0,0 +1,18 @@
+import os
+import winreg
+
+def getUnityPath():
+    regkey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'SOFTWARE\\Unity Technologies\\Installer\\Unity')
+    path = winreg.QueryValueEx(regkey, 'Location x64')[0]
+    path = os.path.join(path, 'Editor', 'Unity.exe')
+    return path.replace('\\', '/')
+
+#unityPath = '"D:/Program Files/Unity/Editor/Unity.exe"'
+unityPath = '"%s"' % getUnityPath()
+print(unityPath)
+
+projectPath = os.path.abspath('./test')
+targetPath = os.path.abspath('./test_auto.exe')
+cmd = '%s -batchmode -projectPath %s -buildWindowsPlayer %s -quit' % (unityPath, projectPath, targetPath)
+print(cmd)
+os.system(cmd)

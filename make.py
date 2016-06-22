851d2e682ec1ef436e5ad35ce7a636029b9cc8df
Anatoly Ahmedov <ludddddd@gmail.com>
2016-06-22 19:01:29 +0300

    save win32 build to zip

--- make.py
+++ make.py
@@ -3,6 +3,7 @@ try:
     import _winreg as winreg
 except ImportError:
     import winreg as winreg
+import zipfile	
     
 
 def getUnityPath():
@@ -15,8 +16,27 @@ def getUnityPath():
 unityPath = '"%s"' % getUnityPath()
 print(unityPath)
 
+targetName = './test_auto'
+
+def getExeName(name):
+	return name + '.exe'
+	
+def getDataFolderName(name):
+	return name + '_Data'
+
 projectPath = os.path.abspath('./test')
-targetPath = os.path.abspath('./test_auto.exe')
-cmd = '%s -batchmode -projectPath %s -buildWindowsPlayer %s -quit' % (unityPath, projectPath, targetPath)
+cmd = '%s -batchmode -projectPath %s -buildWindowsPlayer %s -quit' % (unityPath, projectPath, getExeName(targetName))
 print(cmd)
 os.system(cmd)
+
+def packResult(name):
+	z = zipfile.ZipFile('build_win32.zip', 'w')
+	z.write(getExeName(name))
+	for root, _, filenames in os.walk(getDataFolderName(name)):
+		for name in filenames:
+			name = os.path.join(root, name)
+			name = os.path.normpath(name)
+			z.write(name, name)
+	z.close()
+	
+packResult(targetName)	
